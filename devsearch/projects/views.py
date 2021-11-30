3e8020a2b8eb720d9f216dfb533a843d37be995b
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2021-11-30 16:48:26 -0300

    começando a adicionar meus templates.html

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Aqui está nossos produtos')
+    return render(request, 'projects.html')
 
 
 def project(request, pk):
-    return HttpResponse('Single Project' + ' ' + f'{pk}')
+    return render(request, 'single-project.html')
