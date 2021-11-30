0f1e51c19ca70bdda989532791c9d4450aa90504
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2021-11-30 16:41:25 -0300

    organizando meus paths

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def projects(request):
-    return HttpResponse('Aqui está nossos produtos')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projects, name="projects"),
+    path('', include('projects.urls'))
 
 ]
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def projects(request):
+    return HttpResponse('Aqui está nossos produtos')
+
+
+def project(request, pk):
+    return HttpResponse('Single Project' + ' ' + f'{pk}')
