1fc1fba2422adc6580ab3200c05c3c79ae58dba5
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2021-11-30 16:28:23 -0300

    Criando novo caminho em urls

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse('Aqui está nossos produtos')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', projects, name="projects"),
 
 ]
